439260ffd7f6df05133f09d4718a2380808029bd
Achour Amine <47825029+AchAmine@users.noreply.github.com>
2022-05-09 21:01:16 +0100

    updated chat

--- src/main/java/com/prospace/spring/entity/ChatMessage.java
+++ src/main/java/com/prospace/spring/entity/ChatMessage.java
@@ -29,8 +29,6 @@ public class ChatMessage {
 	   private String chatId;
 	   private String senderId;
 	   private String recipientId;
-	   private String senderName;
-	   private String recipientName;
 	   private String content;
 	   private Date timestamp;
 	   private MessageStatus status;
